b5641773429de592d7cac570e40f36224d0eaf5b
Thinh Luu <quocthinhluu97@gmail.com>
2025-04-26 14:46:57 +0700

    Add verification

--- pages/playwright/document-search.page.ts
+++ pages/playwright/document-search.page.ts
@@ -1,15 +1,21 @@
-import { Locator, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 
 export class DocumentSearchPage {
     readonly page: Page;
 
     readonly searchInput: Locator;
     readonly searchPopup: Locator;
+    readonly navBarTitle: Locator;
+    readonly searchHitItems: Locator;
+    readonly noResultsFoundPopup: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.searchInput = page.locator('text=Search');
         this.searchPopup = page.locator('#docsearch-input');
+        this.navBarTitle = page.locator('.navbar__title');
+        this.searchHitItems = page.locator('#docsearch-list .DocSearch-Hit-title >> text="text"');
+        this.noResultsFoundPopup = page.locator('.DocSearch-NoResults');
     }
 
     async searchTerm(term: string) {
@@ -19,5 +25,17 @@ export class DocumentSearchPage {
 
     async navigate() {
         await this.page.goto('https://playwright.dev/');
+    }
+
+    async verifyNavBarTitle() {
+        await expect(this.navBarTitle).toHaveText('Playwright');
+    }
+
+    async verifyResultsFound() {
+        await expect(this.searchHitItems.first()).toBeVisible();
+    }
+
+    async verifyNoResultsFound() {
+        await expect(this.noResultsFoundPopup).toBeVisible();
     }
 }
--- state-machines/playwright/document-search.machine.ts
+++ state-machines/playwright/document-search.machine.ts
@@ -1,6 +1,7 @@
 import { createModel } from "@xstate/test";
 import { createMachine } from "xstate";
 import { documentSearchEvent } from "./document-search.event";
+import { DocumentSearchPage } from "../../pages/playwright/document-search.page";
 
 const documentSearchMachine = createMachine({
     predictableActionArguments: true,
@@ -11,21 +12,41 @@ const documentSearchMachine = createMachine({
             on: {
                 PERFORM_SEARCH: 'resultsFound',
                 PERFORM_INVALID_SEARCH: 'noResults',
+            },
+            meta: {
+                test: async (documentSearchPage: DocumentSearchPage) => {
+                    await documentSearchPage.verifyNavBarTitle(); 
+                }
             }
         },
         resultsFound: {
             on: {
                 RESET_SEARCH: 'readyForNextSearch',
+            },
+            meta: {
+                test: async (documentSearchPage: DocumentSearchPage) => {
+                    await documentSearchPage.verifyResultsFound(); 
+                }
             }
         },
         noResults: {
             on: {
                 RESET_SEARCH: 'readyForNextSearch',
+            },
+            meta: {
+                test: async (documentSearchPage: DocumentSearchPage) => {
+                    await documentSearchPage.verifyNoResultsFound(); 
+                }
             }
         },
         readyForNextSearch: {
-            type: 'final'
-        }
+            type: 'final',
+            meta: {
+                test: async (documentSearchPage: DocumentSearchPage) => {
+                    await documentSearchPage.verifyNavBarTitle(); 
+                }
+            }
+        },
     }
 });
 
