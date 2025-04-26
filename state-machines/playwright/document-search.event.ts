4384380f4fcbab868a2d1ede0f8dc27182c4160b
Thinh Luu <quocthinhluu97@gmail.com>
2025-04-26 14:17:01 +0700

    Remove unused code

--- state-machines/playwright/document-search.event.ts
+++ state-machines/playwright/document-search.event.ts
@@ -1,19 +1,15 @@
-import { Page } from "@playwright/test";
 import { DocumentSearchPage } from "../../pages/playwright/document-search.page";
 
 const documentSearchEvent = ({
     PERFORM_SEARCH: async (documentSearchPage: DocumentSearchPage) => {
         const validSearchTerm = 'Text selector';
-        // let documentSearch = new DocumentSearchPage(page);
         await documentSearchPage.searchTerm(validSearchTerm);
     },
     PERFORM_INVALID_SEARCH: async (documentSearchPage: DocumentSearchPage) => {
         const invalidSearchTerm = 'pztr';
-        // let documentSearch = new DocumentSearchPage(page);
         await documentSearchPage.searchTerm(invalidSearchTerm);
     },
     RESET_SEARCH: async (documentSearchPage: DocumentSearchPage) => {
-        // let documentSearch = new DocumentSearchPage(page);
         await documentSearchPage.navigate();
     },
 });
